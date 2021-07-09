d252d233778ee725c41cca11c0bd497ad106ab53
garammaru68 <63288078+garammaru68@users.noreply.github.com>
2021-07-09 13:08:14 +0900

    Effect

---
+++ Source/KGCA38/TeamEffect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "TeamEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KGCA38_API UTeamEffect : public UParticleSystemComponent
+{
+	GENERATED_BODY()
+	
+};
