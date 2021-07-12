4d1cb6e9853f50a55055ad355ad517fd9a3a36ef
Young-hyun Kim <kyhkingking@naver.com>
2021-07-12 12:25:37 +0900

    1

--- Source/KGCA38/KGCA38.Build.cs
+++ Source/KGCA38/KGCA38.Build.cs
@@ -8,7 +8,7 @@ public class KGCA38 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","MazeRunnerMapToolPlugins", "TeamAI", "ProjectUI", "TeamEffect", "TeamPlayer" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
